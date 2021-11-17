96cfe750b70d3d050d3c7b2c76d45e8b127087b6
ujin-Bang <qkddnwls14@gmail.com>
2021-11-17 10:07:07 +0900

    코틀린 반복문 (for) 체험

--- app/src/main/java/com/neppplus/lottosimullater/MainActivity.kt
+++ app/src/main/java/com/neppplus/lottosimullater/MainActivity.kt
@@ -2,6 +2,7 @@ package com.neppplus.lottosimullater
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,7 +11,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setValues()
         setupEvents()
-
+        makeLottoNumbers()
 
     }
 
@@ -23,6 +24,17 @@ class MainActivity : AppCompatActivity() {
 
 //            보너스 번호생성
 
+        }
+
+    }
+
+    fun makeLottoNumbers(){
+
+//        6개의 당첨번호 => 반복 횟수 명확 =>for
+
+        for ( i in 0 until 6) {
+
+            Log.d("숫자확인", i.toString())
         }
 
     }

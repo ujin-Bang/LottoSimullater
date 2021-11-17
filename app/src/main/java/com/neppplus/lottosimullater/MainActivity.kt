30ae526a3971dd2417882ece70ce1c44b7b51a5c
ujin-Bang <qkddnwls14@gmail.com>
2021-11-17 11:14:20 +0900

    당첨 금액 합산   - NumberFormat 클래스 다시 활용 (직방)

--- app/src/main/java/com/neppplus/lottosimullater/MainActivity.kt
+++ app/src/main/java/com/neppplus/lottosimullater/MainActivity.kt
@@ -56,7 +56,6 @@ class MainActivity : AppCompatActivity() {
 //        1천원 사용으로 간주
         mUseMoney += 1000
 
-        txtUseMoney.text = "${NumberFormat.getInstance(Locale.KOREA).format(mUseMoney)}원"
 
 //        내 숫자 6개가 -> 당첨번호 6개중 몇개나 맞췄는가?
         var correctCount = 0
@@ -77,30 +76,38 @@ class MainActivity : AppCompatActivity() {
         when (correctCount) {
             6 ->{
                 Log.d("등수","1등입니다")
+                mEarnedMoney += 5000000000
             }
             5->{
 
 //                보너스 번호 검사 => 보너스 번호가 내 번호안에 있는가?
                 if(mMyLottoNumArr.contains(mBonusNum)){
                     Log.d("등수","2등")
+
+                    mEarnedMoney += 50000000
                 }
                 else {
                     Log.d("등수","3등")
+                    mEarnedMoney += 2000000
                 }
 
             }
             4->{
                 Log.d("등수","4등입니다.")
+                mEarnedMoney += 50000
             }
             3->{
                 Log.d("등수","5등입니다.")
+                mUseMoney -= 5000
             }
             else -> {
                 Log.d("등수","꽝")
             }
         }
 
+        txtUseMoney.text = "${NumberFormat.getInstance(Locale.KOREA).format(mUseMoney)}원"
 
+        txtEarnedMoney.text ="${NumberFormat.getInstance(Locale.KOREA).format(mEarnedMoney)}원"
 
 
     }
